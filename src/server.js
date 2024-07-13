25bc2ef7c205a3bf3aecfe76381abc6e782fb86c
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2024-07-13 14:45:15 +0300

    fixed status

--- src/server.js
+++ src/server.js
@@ -26,14 +26,14 @@ export const setupServer = () => {
     try {
       const contacts = await getAllContacts();
       res.status(200).json({
-        status: '200',
+        status: 200,
         message: 'Successfully found contacts!',
         data: contacts,
       });
     } catch (error) {
       console.error(error);
       res.status(500).json({
-        status: '500',
+        status: 500,
         message: 'Internal Server Error!',
       });
     }
@@ -45,20 +45,20 @@ export const setupServer = () => {
       const contact = await getContactById(contactId);
       if (!contact) {
         res.status(404).json({
-          status: '404',
+          status: 404,
           message: 'Contact not found',
         });
         return;
       }
       res.status(200).json({
-        status: '200',
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     } catch (error) {
       console.error(error);
       res.status(500).json({
-        status: '500',
+        status: 500,
         message: 'Internal Server Error!',
       });
     }
