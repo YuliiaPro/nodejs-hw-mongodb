d7834e7adf3fe450ec5294cfe64b6417c470fe2d
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2024-07-20 07:12:48 +0300

    changed branch hw3-crud

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { ContactsCollection } from './db/models/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,58 +25,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        status: 500,
-        message: 'Internal Server Error!',
-      });
-    }
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
-      if (!contact) {
-        res.status(404).json({
-          status: 404,
-          message: 'Contact not found',
-        });
-        return;
-      }
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        status: 500,
-        message: 'Internal Server Error!',
-      });
-    }
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((req, res, next) => {
-    res.status(404).json({ message: 'Not Found' });
-  });
-
-  app.use((error, req, res, next) => {
-    console.error(error);
-    res.status(500).json({
-      message: 'Internal Server Error!',
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
