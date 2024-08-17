bf580d21857f4a903ac1c3ba22ab31829811f210
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2024-08-17 11:50:38 +0300

    fixed serverjs

--- src/server.js
+++ src/server.js
@@ -30,23 +30,14 @@ export const setupServer = () => {
   );
 
   app.use(router);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-};
-
-export const startServer = () => {
-  const app = express();
-
-  app.use(express.json());
-  app.use(cors());
-  app.use(cookieParser());
 };
